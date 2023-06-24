70e2b9512a519ca6e871f076d55be2b3ea288c95
muazzam <muazzamafaque@gmail.com>
2023-06-24 16:34:58 +0500

    basic functionality working fine now

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ async function fetchFurrorProducts() {
 
     const saleProductsSelector = '.CategoryPage-ProductListWrapper > div'
     const productCardSelector = '.ProductCard'
-    const titleSelector = '.ProductCard-Content > p'
+    const titleSelector = '.ProductCard-Content > p.ProductCard-Name'
     const imgSelector = '.ProductCard-FigureReview > figure > div > img'
     const productLinkSelector = 'a'
     const discountPriceSelector = '.ProductPrice span'
@@ -42,6 +42,8 @@ async function fetchFurrorProducts() {
 
     const saleProductHandles = await page.$$(productCardSelector);
 
+    console.log(saleProductHandles.length)
+
     for (const saleProduct of saleProductHandles) {
 
         //setting default values of each product in case if something goes wrong
@@ -53,39 +55,45 @@ async function fetchFurrorProducts() {
 
         try {
             title = await page.evaluate(
-                (el, selector) => el.querySelector(selector)?.innerHTML || '',
+                (el, selector) => el.querySelector(selector).innerHTML,
                 saleProduct,
                 titleSelector
             );
-
+            // console.log("title", title)
             img = await page.evaluate(
-                (el, selector) => el.querySelector(selector)?.getAttribute("src") || '',
+                (el, selector) => el.querySelector(selector).getAttribute("src"),
                 saleProduct,
                 imgSelector
             );
+            // console.log("img", img)
+
 
             productLink = await page.evaluate(
-                (el, selector, baseUrl) => baseUrl + el.querySelector(selector)?.getAttribute("href") || '',
+                (el, selector, baseUrl) => baseUrl + el.querySelector(selector).getAttribute("href"),
                 saleProduct,
                 productLinkSelector,
                 furrorBaseUrl
             );
+            // console.log("productLink", productLink)
 
             discountPrice = await page.evaluate(
-                (el, selector) => el.querySelector(selector)?.textContent || '',
+                (el, selector) => el.querySelector(selector).textContent,
                 saleProduct,
                 discountPriceSelector
             );
+            // console.log("discountPrice", discountPrice)
 
             originalPrice = await page.evaluate(
-                (el, selector) => el.querySelector(selector)?.textContent || '',
+                (el, selector) => el.querySelector(selector).textContent,
                 saleProduct,
                 originalPriceSelector
             );
+            // console.log("original price", originalPrice)
+
 
 
         } catch (error) {
-            console.error(error)
+            console.error('error is', error) //ignore the errors that occur due to these now.
         }
 
         if (title.length != 0 && img.length != 0 && productLink.length != 0 && discountPrice.length != 0 && originalPrice.length != 0) {
@@ -95,6 +103,7 @@ async function fetchFurrorProducts() {
         }
     }
     //close the browser after fetching all the products
+    console.log(saleProducts)
     await browser.close()
 }
 
@@ -125,6 +134,35 @@ app.get('/api/furror', async (req, res) => {
         res.status(500).json({ error: 'Failed to fetch Furror products' });
     }
 })
+
+// // the request will look like this: /api/furror?pages=3
+
+// app.get('/api/furror', async (req, res) => {
+//     try {
+//         let numPages = 1;
+//         if (req.query.pages != null) {
+//             numPages = parseInt(req.query.pages);
+//             if (isNaN(numPages) || numPages < 1) {
+//                 res.status(400).json({ error: 'Invalid number of pages' });
+//                 return;
+//             }
+//         }
+
+//         const totalPages = await getTotalPages();
+//         if (totalPages < numPages) {
+//             res.status(400).json({ error: `Only ${totalPages} pages available` });
+//             return;
+//         }
+
+//         saleProducts = [];
+//         await fetchFurrorProducts(numPages);
+//         res.json(saleProducts);
+//     } catch (error) {
+//         console.error('Error fetching Furror products:', error);
+//         res.status(500).json({ error: 'Failed to fetch Furror products' });
+//     }
+// });
+
 
 app.listen(PORT, () => {
     console.log(`server running on ${PORT}`)
